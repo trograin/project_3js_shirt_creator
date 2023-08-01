18bef5e20938bc2102479144995c06c3978c2c53
boban.alempijevic <boban.alempijevic@gmail.com>
2023-08-01 22:04:22 +0300

    updated adresses to correct ones for production side

--- server/index.js
+++ server/index.js
@@ -16,4 +16,4 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: "Hello from DALL.E" })
 })
 
-app.listen(8080, () => console.log('Server has started on port 8080'))
+app.listen(7070, () => console.log('Server has started on port 7070'))
